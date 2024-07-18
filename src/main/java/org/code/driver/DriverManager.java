887a918d6106dd4fb8f3ee6ccfe6c83122a89116
mahesh2107 <maheshgoud2107@gmail.com>
2024-07-18 11:38:15 +0530

    18 july 2024

--- src/main/java/org/code/driver/DriverManager.java
+++ src/main/java/org/code/driver/DriverManager.java
@@ -1,4 +1,22 @@
 package org.code.driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeMethod;
+
+
+
+
 public class DriverManager {
+    static WebDriver driver;
+
+
+    @BeforeMethod
+    static void init() {
+        if (driver == null) {
+            driver = new EdgeDriver();
+        }
+    }
+
+
 }
